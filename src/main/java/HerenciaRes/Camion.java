c650190340f3185b3516ab6c66090d77431b69dc
Prof Matias Garcia <54009386+profmatiasgarcia@users.noreply.github.com>
2021-02-18 19:58:22 -0300

    Add files via upload

---
+++ src/main/java/HerenciaRes/Camion.java
@@ -0,0 +1,34 @@
+package HerenciaRes;
+/**
+ * @author Prof Matias Garcia.
+ * <p> Copyright (C) 2017 para <a href = "https://www.profmatiasgarcia.com.ar/"> www.profmatiasgarcia.com.ar </a>
+ * - con licencia GNU GPL3.
+ * <p> Este programa es software libre. Puede redistribuirlo y/o modificarlo bajo los términos de la
+ * Licencia Pública General de GNU según es publicada por la Free Software Foundation, 
+ * bien con la versión 3 de dicha Licencia o bien (según su elección) con cualquier versión posterior. 
+ * Este programa se distribuye con la esperanza de que sea útil, pero SIN NINGUNA GARANTÍA, 
+ * incluso sin la garantía MERCANTIL implícita o sin garantizar la CONVENIENCIA PARA UN PROPÓSITO
+ * PARTICULAR. Véase la Licencia Pública General de GNU para más detalles.
+ * Debería haber recibido una copia de la Licencia Pública General junto con este programa. 
+ * Si no ha sido así ingrese a <a href = "http://www.gnu.org/licenses/"> GNU org </a>
+ */
+public class Camion extends Vehiculo {
+
+    public double tara;
+
+    public Camion() {
+        //sentencias constructor Camion
+    }
+
+    public Camion(double tara, String patente, double kilometraje) {
+        super(patente, kilometraje);
+        this.tara = tara;
+    }
+
+    @Override
+    public void mostrarCaracteristicas() {
+        System.out.println("Tara: " + this.tara);
+        System.out.println("Patente: " + this.getPatente());
+        System.out.println("Kilometraje: " + this.getKilometraje());
+    }
+}
---
+++ src/main/java/HerenciaRes/Vehiculo.java
@@ -0,0 +1,52 @@
+package HerenciaRes;
+/**
+ * @author Prof Matias Garcia.
+ * <p> Copyright (C) 2017 para <a href = "https://www.profmatiasgarcia.com.ar/"> www.profmatiasgarcia.com.ar </a>
+ * - con licencia GNU GPL3.
+ * <p> Este programa es software libre. Puede redistribuirlo y/o modificarlo bajo los términos de la
+ * Licencia Pública General de GNU según es publicada por la Free Software Foundation, 
+ * bien con la versión 3 de dicha Licencia o bien (según su elección) con cualquier versión posterior. 
+ * Este programa se distribuye con la esperanza de que sea útil, pero SIN NINGUNA GARANTÍA, 
+ * incluso sin la garantía MERCANTIL implícita o sin garantizar la CONVENIENCIA PARA UN PROPÓSITO
+ * PARTICULAR. Véase la Licencia Pública General de GNU para más detalles.
+ * Debería haber recibido una copia de la Licencia Pública General junto con este programa. 
+ * Si no ha sido así ingrese a <a href = "http://www.gnu.org/licenses/"> GNU org </a>
+ */
+public abstract class Vehiculo {
+
+    protected String patente;
+    protected double kilometraje;
+
+    public Vehiculo() {
+        //sentencias constructor
+    }
+
+    public Vehiculo(String patente, double kilometraje) {
+        this.patente = patente;
+        this.kilometraje = kilometraje;
+    }
+
+    public String getPatente() {
+        return patente;
+    }
+
+    public void setPatente(String patente) {
+        this.patente = patente;
+    }
+
+    public double getKilometraje() {
+        return kilometraje;
+    }
+
+    public void setKilometraje(double kilometraje) {
+        this.kilometraje = kilometraje;
+    }
+
+    public void mostrarCaracteristicas() {
+        //sentencias metodo
+        System.out.println("Patente: " + this.getPatente());
+        System.out.println("Kilometraje: " + this.getKilometraje());
+        System.out.println("");
+    }
+
+}
